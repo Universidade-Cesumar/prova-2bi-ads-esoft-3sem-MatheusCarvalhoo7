b0ee91cd4181c1c077bb36ffa4a51efa452b644f
Matheus Carvalho <mc227443@gmail.com>
2026-06-10 20:44:47 -0300

    Implementa listagem de materiais via API

--- main.js
+++ main.js
@@ -6,3 +6,26 @@ const inputNome = document.getElementById("input-nome");
 const inputQuantidade = document.getElementById("input-quantidade");
 const btnCadastrar = document.getElementById("btn-cadastrar");
 const listaMateriais = document.getElementById("lista-materiais");
+
+async function listarMateriais() {
+
+    const resposta = await fetch(url);
+
+    const materiais = await resposta.json();
+
+    listaMateriais.innerHTML = "";
+
+    materiais.forEach(material => {
+
+        listaMateriais.innerHTML += `
+            <tr>
+                <td>${material.nome}</td>
+                <td>${material.quantidade}</td>
+            </tr>
+        `;
+
+    });
+
+}
+
+listarMateriais();
