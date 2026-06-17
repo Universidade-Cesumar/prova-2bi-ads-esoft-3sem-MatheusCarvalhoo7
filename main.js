48e51e011ebf644d49620acc043b667045dae96d
Matheus Carvalho <mc227443@gmail.com>
2026-06-17 19:27:52 -0300

    Implementa validacao de retirada de estoque

--- main.js
+++ main.js
@@ -52,3 +52,16 @@ async function cadastrarMaterial() {
 }
 
 btnCadastrar.addEventListener("click", cadastrarMaterial);
+
+function validarRetirada(estoqueAtual, quantidadeRetirada) {
+
+    if (quantidadeRetirada <= 0) {
+        return false;
+    }
+
+    if (quantidadeRetirada > estoqueAtual) {
+        return false;
+    }
+
+    return true;
+}
